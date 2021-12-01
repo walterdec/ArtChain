ced826c0762cb995894d52e7786f75d4c35681f3
Walter <walter.decarne@gmail.com>
2021-12-01 18:36:11 +0100

    Artist and Wallet classes

---
+++ Artist.py
@@ -0,0 +1,58 @@
+from Wallet import Wallet
+
+
+class Artist(object):
+    def __init__(self, username, password, name, surname, is_musician):
+        self.username = username
+        self.password = password
+        self.name = name #optional
+        self.surname = surname #optional
+        self.is_musician = is_musician
+        #init_social_media()
+        wallet = Wallet(object)
+
+    insta = 0
+    face = 0
+    twitter = 0
+    yt= 0
+    tiktok = 0
+    twitch = 0
+
+    applemusic = 0
+    spotify = 0
+    soundcloud = 0
+
+    sales = 0.0
+
+def evaluation(insta, face, twitter, yt, tiktok, twitch, applemusic, spotify, soundcloud, sales):
+    insta = insta
+    face = face
+    twitter = twitter
+    yt = yt
+    tiktok=tiktok
+    twitch = twitch
+    applemusic = applemusic
+    spotify = spotify
+    soundcloud = soundcloud
+    sales = sales
+    if is_musician:
+        crypto_value = (insta*0.3 + face*0.2 + twitter*0.2 + yt*0.1 + tiktok*0.1 +
+         twitch*0.1)*0.5 + (applemusic*0.4 + spotify*0.5 + soundcloud*0.1)*0.3 + sales*0.2
+    else:
+        crypto_value = (insta*0.3 + face*0.2 + twitter*0.2 + yt*0.1 + tiktok*0.1 +
+         twitch*0.1)*0.8 + sales*0.2
+
+
+
+
+crypto_value = 0.0
+
+
+
+
+
+
+
+
+
+
---
+++ Wallet.py
@@ -0,0 +1,5 @@
+class Wallet(object):
+    def __init__(self, id):
+        self.id = id
+        self.ac_crypto_amount = 0
+
